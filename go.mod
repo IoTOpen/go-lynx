f9808dfdd1c14bf0307879b7028b1ac758f09b4a
Jesper Olofsson <jesper.olofsson@indentive.se>
2021-07-05 15:59:09 +0200

    added a functions for creating default mqtt options

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.15
 
 require (
 	github.com/eclipse/paho.mqtt.golang v1.3.3
+	github.com/spf13/viper v1.8.1
 	golang.org/x/net v0.0.0-20210415231046-e915ea6b2b7d // indirect
 )
--- mqtt.go
+++ mqtt.go
@@ -3,6 +3,9 @@ package lynx
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/eclipse/paho.mqtt.golang"
+	"github.com/spf13/viper"
+	"log"
 	"time"
 )
 
@@ -31,4 +34,32 @@ func (c *Client) Publish(topic string, payload interface{}, qos byte) error {
 	token := c.Mqtt.Publish(topic, qos, false, data)
 	token.WaitTimeout(time.Second)
 	return token.Error()
+}
+
+func NewMqttOptions(conf *viper.Viper, onConnect mqtt.OnConnectHandler, onLost mqtt.ConnectionLostHandler) *mqtt.ClientOptions {
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(conf.GetString("broker"))
+	opts.SetClientID(conf.GetString("client_id"))
+	opts.SetCleanSession(true)
+	opts.SetConnectRetryInterval(time.Second * 5)
+	opts.SetConnectTimeout(time.Second)
+	opts.SetAutoReconnect(true)
+
+	opts.SetConnectionLostHandler(func(c mqtt.Client, err error) {
+		if conf.GetBool("connection_log") {
+			log.Println("MQTT: connection lost:", err.Error())
+		}
+		if onLost != nil {
+			onLost(c, err)
+		}
+	})
+	opts.SetOnConnectHandler(func(c mqtt.Client) {
+		if conf.GetBool("connection_log") {
+			log.Println("MQTT: connected")
+		}
+		if onConnect != nil {
+			onConnect(c)
+		}
+	})
+	return opts
 }
