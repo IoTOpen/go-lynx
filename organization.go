9ab4b76f4631190d2ac7415eaf37d22b709794c9
Jesper Olofsson <46094983+Thottbot@users.noreply.github.com>
2021-08-30 15:02:28 +0200

    Organization (#18)
    * added organization endpoints
    
    * removed leading slash in path
    
    * review fix
    
    Co-authored-by: Jesper Olofsson <jesper.olofsson@indentive.se>

---
+++ organization.go
@@ -0,0 +1,64 @@
+package lynx
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Organization struct {
+	ID            int64                `json:"id"`
+	Name          string               `json:"name"`
+	Address       Address              `json:"address"`
+	Email         string               `json:"email"`
+	Phone         string               `json:"phone"`
+	ForceSMSLogin bool                 `json:"force_sms_login"`
+	Parent        int64                `json:"parent"`
+	Children      []*OrganizationChild `json:"children"`
+	Notes         string               `json:"notes"`
+	Meta          map[string]string    `json:"meta"`
+	ProtectedMeta map[string]string    `json:"protected_meta"`
+}
+
+type OrganizationChild struct {
+	Name string `json:"name"`
+	ID   int64  `json:"id"`
+}
+
+func (c *Client) ListOrganization(minimal bool) ([]*Organization, error) {
+	res := make([]*Organization, 0, 5)
+	path := fmt.Sprintf("api/v2/organization?minimal=%t", minimal)
+	req := c.newRequest(http.MethodGet, path, nil)
+	if err := c.do(req, &res); err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+func (c *Client) GetOrganization(organizationID int64) (*Organization, error) {
+	res := &Organization{}
+	path := fmt.Sprintf("api/v2/organization/%d", organizationID)
+	req := c.newRequest(http.MethodGet, path, nil)
+	if err := c.do(req, res); err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+func (c *Client) CreateOrganization(org *Organization) (*Organization, error) {
+	organization := &Organization{}
+	req := c.newRequest(http.MethodPost, "api/v2/organization", requestBody(org))
+	if err := c.do(req, organization); err != nil {
+		return nil, err
+	}
+	return organization, nil
+}
+
+func (c *Client) UpdateOrganization(org *Organization) (*Organization, error) {
+	organization := &Organization{}
+	path := fmt.Sprintf("api/v2/organization/%d", org.ID)
+	req := c.newRequest(http.MethodPut, path, requestBody(org))
+	if err := c.do(req, organization); err != nil {
+		return nil, err
+	}
+	return organization, nil
+}
