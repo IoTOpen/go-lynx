22d213bc671adaa149a720e45b634a37d7b7933d
Felix Jontegen <felix.jontegen@th1ng.se>
2026-05-11 15:30:31 +0200

    fix backwards compability

--- auth.go
+++ auth.go
@@ -25,18 +25,18 @@ func (b Basic) SetMQTTAuth(o *mqtt.ClientOptions) {
 	o.SetPassword(b.Password)
 }
 
-type AuthApiKey struct {
+// Deprecated: use AuthAPIKey.
+type AuthApiKey = AuthAPIKey
+
+type AuthAPIKey struct {
 	Key string
 }
 
-// AuthAPIKey is an alias for AuthApiKey to avoid confusion with the struct name
-type AuthAPIKey = AuthApiKey
-
-func (a AuthApiKey) SetHTTPAuth(r *http.Request) {
+func (a AuthAPIKey) SetHTTPAuth(r *http.Request) {
 	r.Header.Set("X-API-Key", a.Key)
 }
 
-func (a AuthApiKey) SetMQTTAuth(o *mqtt.ClientOptions) {
+func (a AuthAPIKey) SetMQTTAuth(o *mqtt.ClientOptions) {
 	o.SetUsername("apikey")
 	o.SetPassword(a.Key)
 }
